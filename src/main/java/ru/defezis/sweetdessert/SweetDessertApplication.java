13a9bd32feebffc1c68dd85386b832f6a073680d
Artem Mikhalko <amikhalko@geosteering.ru>
2024-04-02 21:57:03 +0500

    initial commit

---
+++ src/main/java/ru/defezis/sweetdessert/SweetDessertApplication.java
@@ -0,0 +1,13 @@
+package ru.defezis.sweetdessert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetDessertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetDessertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/defezis/sweetdessert/SweetDessertApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.defezis.sweetdessert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetDessertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
