afb20d947d62abf2140c73c998150d6f6fff839d
Artem Mikhalko <amikhalko@geosteering.ru>
2024-07-16 23:59:31 +0500

    007: правки

--- src/main/java/ru/defezis/sweetdessert/web/controller/LoginController.java
+++ src/main/java/ru/defezis/sweetdessert/web/controller/LoginController.java
@@ -3,7 +3,6 @@ package ru.defezis.sweetdessert.web.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.defezis.sweetdessert.dto.CreateUserRequest;
